d83ac25c321e09b65381f010aa87d899cf1fb295
Guilherme B. Pinheiro <guilhermepinheiro@gmail.com>
2019-12-17 18:49:17 -0300

    Create Hello World endpoint

--- src/main/java/dev/guilhermebpnr/springbootlearning/SpringBootLearningApplication.java
+++ src/main/java/dev/guilhermebpnr/springbootlearning/SpringBootLearningApplication.java
@@ -2,12 +2,45 @@ package dev.guilhermebpnr.springbootlearning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringBootLearningApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootLearningApplication.class, args);
+	}
+
+	@RestController
+	class Resource {
+
+		@RequestMapping(
+				method = RequestMethod.GET
+		)
+		Message getMessage() {
+			return new Message("Hello World!");
+		}
+	}
+
+	class Message {
+		private final String message;
+
+		public Message(String message) {
+			this.message = message;
+		}
+
+		public String getMessage() {
+			return message;
+		}
+
+		@Override
+		public String toString() {
+			return "Message{" +
+					"message='" + message + '\'' +
+					'}';
+		}
 	}
 
 }
