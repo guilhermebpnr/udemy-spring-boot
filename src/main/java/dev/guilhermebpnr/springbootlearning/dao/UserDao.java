35571f5d76161bc11adaeb0629a90c612e161d69
Guilherme B. Pinheiro <guilhermepinheiro@gmail.com>
2019-12-18 15:35:15 -0300

    Add UserDao interface

--- src/main/java/dev/guilhermebpnr/springbootlearning/dao/UserDao.java
+++ src/main/java/dev/guilhermebpnr/springbootlearning/dao/UserDao.java
@@ -1,4 +1,19 @@
 package dev.guilhermebpnr.springbootlearning.dao;
 
-public class UserDao {
+import dev.guilhermebpnr.springbootlearning.model.User;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface UserDao {
+
+    List<User> getAllUsers();
+
+    User getUser(UUID userUid);
+
+    int updateUser(User user);
+
+    int removerUser(UUID userUid);
+
+    int insertUser(User user);
 }
