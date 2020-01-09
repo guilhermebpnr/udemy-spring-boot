2eed6bb396a421c6a99c891b6c7dc204de428eee
Guilherme B. Pinheiro <guilhermepinheiro@gmail.com>
2020-01-08 22:58:16 -0300

    Configure JBoss RESTEasy JAX RS Client for integration tests

---
+++ src/main/java/dev/guilhermebpnr/springbootlearning/clientproxy/UserResourceV1.java
@@ -0,0 +1,35 @@
+package dev.guilhermebpnr.springbootlearning.clientproxy;
+
+import dev.guilhermebpnr.springbootlearning.model.User;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.UUID;
+
+public interface UserResourceV1 {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    List<User> fetchUsers(@QueryParam("gender") String gender);
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{userUid}")
+    Response fetchUser(@PathParam("userUid") UUID userUid);
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    Response insertUser(User user);
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    Response updateUser(User user);
+
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{userUid}")
+    Response removeUser(@PathParam("userUid") UUID userUid);
+}
